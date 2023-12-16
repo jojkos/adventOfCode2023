fd4a159f1c325836056ec6f885be7114cba841ed
Jonas Holcner <jojkos@gmail.com>
2023-12-16 06:58:02 +0100

    day 16 first half

---
+++ 16/code.ts
@@ -0,0 +1,162 @@
+import { parseFileIntoLines } from "../utils";
+
+const inputLines = parseFileIntoLines("input.txt");
+const map = inputLines.map(line => line.split(""));
+
+const Empty = ".";
+
+interface IPos {
+    x: number;
+    y: number;
+}
+
+enum Direction {
+    Up = "Up",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right"
+}
+
+const DirMapping: Record<Direction, IPos> = {
+    [Direction.Up]: { x: 0, y: -1 },
+    [Direction.Down]: { x: 0, y: 1 },
+    [Direction.Right]: { x: 1, y: 0 },
+    [Direction.Left]: { x: -1, y: 0 }
+};
+
+// for each type of tile,
+// for each dir it could be accessed from,
+// next direction
+const ValToDir = {
+    ".": {
+        [Direction.Up]: Direction.Up,
+        [Direction.Down]: Direction.Down,
+        [Direction.Right]: Direction.Right,
+        [Direction.Left]: Direction.Left
+    },
+    "|": {
+        [Direction.Up]: Direction.Up,
+        [Direction.Down]: Direction.Down,
+        [Direction.Right]: [Direction.Down, Direction.Up],
+        [Direction.Left]: [Direction.Down, Direction.Up]
+    },
+    "-": {
+        [Direction.Up]: [Direction.Left, Direction.Right],
+        [Direction.Down]: [Direction.Left, Direction.Right],
+        [Direction.Right]: Direction.Right,
+        [Direction.Left]: Direction.Left
+    },
+    "/": {
+        [Direction.Up]: Direction.Right,
+        [Direction.Down]: Direction.Left,
+        [Direction.Right]: Direction.Up,
+        [Direction.Left]: Direction.Down
+    },
+    "\\": {
+        [Direction.Up]: Direction.Left,
+        [Direction.Down]: Direction.Right,
+        [Direction.Right]: Direction.Down,
+        [Direction.Left]: Direction.Up
+    }
+};
+
+const getTileFromPos = (pos: IPos) => {
+    return map[pos.y][pos.x];
+};
+const posToKey = (pos: IPos) => {
+    return `${pos.y}:${pos.x}`;
+};
+const posAndDirToKey = (pos: IPos, dir: Direction) => {
+    return `${pos.y}:${pos.x}-${dir}`;
+};
+const getNextPosAndDir = (currentPos: IPos, currentDir: Direction) => {
+    const currentTile = getTileFromPos(currentPos);
+    const nextDir = ValToDir[currentTile][currentDir];
+
+
+    if (Array.isArray(nextDir)) {
+        const res = [];
+
+        for (let i = 0; i < nextDir.length; i++) {
+            const dir = nextDir[i];
+            const nextPos = {
+                x: currentPos.x + DirMapping[dir].x,
+                y: currentPos.y + DirMapping[dir].y
+            };
+
+            res.push({
+                pos: nextPos,
+                dir: dir
+            });
+        }
+        return res;
+    } else {
+        const nextPos = {
+            x: currentPos.x + DirMapping[nextDir].x,
+            y: currentPos.y + DirMapping[nextDir].y
+        };
+
+        return {
+            pos: nextPos,
+            dir: nextDir
+        };
+    }
+
+};
+const isOutside = (pos: IPos) => {
+    return pos.x < 0 || pos.y < 0 || pos.y >= map.length || pos.x >= map[0].length;
+};
+
+const beamsCycleCache: Record<string, boolean> = {};
+const energized: Record<string, boolean> = {};
+
+let res1 = 0;
+
+console.time();
+
+type TBeam = { id: number, pos: IPos, dir: Direction };
+
+const beams: TBeam[] = [];
+let maxId = 1;
+let beam: TBeam = { id: maxId, pos: { x: 0, y: 0 }, dir: Direction.Right };
+
+while (beam) {
+    // console.log(`${beam.id}:`,beam.pos, beam.dir);
+
+    if (isOutside(beam.pos) || beamsCycleCache[posAndDirToKey(beam.pos, beam.dir)]) {
+        // console.log("end:", beam.id);
+        beam = beams.shift();
+        continue;
+    }
+
+    beamsCycleCache[posAndDirToKey(beam.pos, beam.dir)] = true;
+    energized[posToKey(beam.pos)] = true;
+
+    const next = getNextPosAndDir(beam.pos, beam.dir);
+
+    if (Array.isArray(next)) {
+        const [first, second] = next;
+
+        beam.pos = first.pos;
+        beam.dir = first.dir;
+
+        maxId += 1;
+
+        if (!isOutside(second.pos)) {
+            beams.push({
+                id: maxId,
+                pos: second.pos,
+                dir: second.dir
+            });
+        }
+
+    } else {
+        beam.pos = next.pos;
+        beam.dir = next.dir;
+    }
+}
+
+console.timeEnd();
+
+console.log(`res1:`, Object.keys(energized).length);
+
